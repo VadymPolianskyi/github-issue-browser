2e060cacb3c9c587e2d8e81f16ace046fe732357
vadym <vadym.polyanski@gmail.com>
2017-11-04 18:43:55 +0200

    Initial commit

---
+++ src/main/java/com/piedpiper/gib/GithubIssueBrowserApplication.java
@@ -0,0 +1,12 @@
+package com.piedpiper.gib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubIssueBrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubIssueBrowserApplication.class, args);
+	}
+}
---
+++ src/main/java/com/piedpiper/gib/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.piedpiper.gib;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GithubIssueBrowserApplication.class);
+	}
+
+}
