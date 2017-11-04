9137a47596afa5ab9e70197108bc74ba6e9ec17b
vadym <vadym.polyanski@gmail.com>
2017-11-05 01:17:00 +0200

    created methods for work with github issues

--- src/main/java/com/piedpiper/gib/service/GithubService.java
+++ src/main/java/com/piedpiper/gib/service/GithubService.java
@@ -1,7 +1,31 @@
 package com.piedpiper.gib.service;
 
+import org.kohsuke.github.*;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
 
 @Service
 public class GithubService {
+
+    public List<GHIssue> getIssues(String user, String repositoryName, GHIssueState state, int page, int size, String token) {
+        PagedIterator<GHIssue> iterator = getRepository(repositoryName, user, token).listIssues(state)._iterator(size);
+        int counter = 0;
+        while (page < counter || !iterator.hasNext()) {
+            if (page == counter) return  iterator.nextPage();
+            iterator.nextPage();
+            counter++;
+        }
+        throw new RuntimeException(); //todo: create custom IssuesNotFoundException
+    }
+
+    private GHRepository getRepository(String name, String user, String token) {
+        try {
+            GitHub github = GitHub.connectUsingOAuth(token);
+            return github.getUser(user).getRepository(name);
+        } catch (IOException e) {
+            throw new RuntimeException();//todo:create custom RepositoryNotFoundException
+        }
+    }
 }
