d9c70de3565696d94007ea9793f118a1f842d02c
vadym <vadym.polyanski@gmail.com>
2017-11-05 01:17:23 +0200

    get issues api

--- src/main/java/com/piedpiper/gib/protocol/GetIssuesRequest.java
+++ src/main/java/com/piedpiper/gib/protocol/GetIssuesRequest.java
@@ -1,10 +1,25 @@
 package com.piedpiper.gib.protocol;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.kohsuke.github.GHIssueState;
 
+@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
-public class GetIssuesRequest extends Request {
-    private String repoId;
+public @Data class GetIssuesRequest extends TokenRequest {
+
+    @JsonIgnore
+    private String repositoryName;
+    @JsonIgnore
+    private String user;
+    @JsonIgnore
+    private int page;
+    @JsonIgnore
+    private int size;
+    @JsonIgnore
+    private GHIssueState state;
 }
