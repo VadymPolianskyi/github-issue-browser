9f1abe90955648237d16688bb41afe742b95f8b6
vadym <vadym.polyanski@gmail.com>
2017-11-05 12:03:38 +0200

    added label mapping

--- src/main/java/com/piedpiper/gib/service/util/Mapper.java
+++ src/main/java/com/piedpiper/gib/service/util/Mapper.java
@@ -1,7 +1,9 @@
 package com.piedpiper.gib.service.util;
 
 import com.piedpiper.gib.protocol.dao.IssueDao;
+import com.piedpiper.gib.protocol.dao.LabelDao;
 import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHLabel;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +13,10 @@ public class Mapper {
     private ModelMapper mapper = new ModelMapper();
 
     public IssueDao mapIssue(GHIssue issue) {
-
         return mapper.map(issue, IssueDao.class);
+    }
+
+    public LabelDao mapLabel(GHLabel label) {
+        return mapper.map(label, LabelDao.class);
     }
 }
