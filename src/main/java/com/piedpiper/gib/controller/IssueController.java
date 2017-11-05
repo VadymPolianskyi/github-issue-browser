6628c387f52bf48ebf39ad31340301d95ca9b264
vadym <vadym.polyanski@gmail.com>
2017-11-05 22:50:39 +0200

    corrected controller for details

--- src/main/java/com/piedpiper/gib/controller/IssueController.java
+++ src/main/java/com/piedpiper/gib/controller/IssueController.java
@@ -2,7 +2,9 @@ package com.piedpiper.gib.controller;
 
 import com.piedpiper.gib.handler.AllLabelsHandler;
 import com.piedpiper.gib.handler.GetIssuesHandler;
+import com.piedpiper.gib.handler.IssueDetailsHandler;
 import com.piedpiper.gib.protocol.GetIssuesRequest;
+import com.piedpiper.gib.protocol.IssueDetailsRequest;
 import com.piedpiper.gib.protocol.Response;
 import com.piedpiper.gib.protocol.TokenRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,11 +20,13 @@ public class IssueController {
 
     private final GetIssuesHandler getIssuesHandler;
     private final AllLabelsHandler allLabelsHandler;
+    private final IssueDetailsHandler issueDetailsHandler;
 
     @Autowired
-    public IssueController(GetIssuesHandler getIssuesHandler, AllLabelsHandler allLabelsHandler) {
+    public IssueController(GetIssuesHandler getIssuesHandler, AllLabelsHandler allLabelsHandler, IssueDetailsHandler issueDetailsHandler) {
         this.getIssuesHandler = getIssuesHandler;
         this.allLabelsHandler = allLabelsHandler;
+        this.issueDetailsHandler = issueDetailsHandler;
     }
 
     @PostMapping("/issues")
@@ -41,6 +45,11 @@ public class IssueController {
     public Response getClosedIssues(@RequestBody GetIssuesRequest request) {
         request.setState(CLOSED);
         return getIssuesHandler.handle(request);
+    }
+
+    @PostMapping("/issues/details")
+    public Response getIssueDetail(@RequestBody IssueDetailsRequest request) {
+        return issueDetailsHandler.handle(request);
     }
 
     @PostMapping("/labels")
