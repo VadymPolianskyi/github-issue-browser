139d4180e61991c5b684b1b8d4f63ee53877e194
vadym <vadym.polyanski@gmail.com>
2017-11-05 10:44:57 +0200

    added issue creation into github service

--- src/main/java/com/piedpiper/gib/service/GithubService.java
+++ src/main/java/com/piedpiper/gib/service/GithubService.java
@@ -1,13 +1,34 @@
 package com.piedpiper.gib.service;
 
+import com.piedpiper.gib.protocol.dao.IssueDao;
 import org.kohsuke.github.*;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.lang.reflect.Constructor;
 import java.util.List;
 
 @Service
 public class GithubService {
+
+    public GHIssue createIssue(IssueDao issueDao, String token) {
+        try {
+            GitHub gitHub = getConnection(token);
+            GHRepository repository = gitHub.getUser(issueDao.getOwner().getOwnerName()).getRepository(issueDao.getOwner().getName());
+
+            GHIssueBuilder issueBuilder = createIssueBuilder(repository, issueDao.getTitle());
+
+            issueDao.getLabels()
+                    .forEach(label -> issueBuilder.label(label.getName()));
+
+            return issueBuilder.assignee(repository.getOwner())
+                    .body(issueDao.getBody()).create();
+        } catch (IOException e) {
+            throw new RuntimeException();//todo: create custom IssueCreationException
+        }
+
+
+    }
 
     public List<GHIssue> getIssues(String user, String repositoryName, GHIssueState state, int page, int size, String token) {
         PagedIterator<GHIssue> iterator = getRepository(repositoryName, user, token).listIssues(state)._iterator(size);
@@ -34,5 +55,18 @@ public class GithubService {
         } catch (IOException e) {
             throw new RuntimeException();//todo:create custom ConnectionEstablishingException
         }
+    }
+
+    private GHIssueBuilder createIssueBuilder(GHRepository repo, String title) {
+        try {
+            Constructor<GHIssueBuilder> constructor = GHIssueBuilder.class
+                    .getDeclaredConstructor(GHRepository.class, String.class);
+            constructor.setAccessible(true);
+            return constructor.newInstance(repo, title);
+        } catch (Exception e) {
+            throw new RuntimeException();// todo: create custom IssueBuilderCreationException;
+        }
+
+
     }
 }
