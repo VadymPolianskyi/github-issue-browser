d86ccf52106cf91709ebd49e3d452d8222092fd8
VadymPolyanski <vadym.polyanski@gmail.com>
2017-11-06 10:33:23 +0200

    added logging into handlers

--- src/main/java/com/piedpiper/gib/handler/AllLabelsHandler.java
+++ src/main/java/com/piedpiper/gib/handler/AllLabelsHandler.java
@@ -5,6 +5,7 @@ import com.piedpiper.gib.protocol.TokenRequest;
 import com.piedpiper.gib.protocol.dao.LabelDao;
 import com.piedpiper.gib.service.GithubService;
 import com.piedpiper.gib.service.util.Mapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
+@Slf4j
 public class AllLabelsHandler implements Handler<TokenRequest, AllLabelsResponse> {
 
     private final GithubService githubService;
@@ -25,11 +27,14 @@ public class AllLabelsHandler implements Handler<TokenRequest, AllLabelsResponse
 
     @Override
     public AllLabelsResponse handle(TokenRequest request) {
+        log.debug("Got request for getting all labels.");
+
         List<LabelDao> labels = githubService.labels(request.getToken())
                 .stream()
                 .map(mapper::mapLabel)
                 .collect(Collectors.toList());
 
+        log.info("Returned list with all labels. Count: {}", labels.size());
         return new AllLabelsResponse(labels);
     }
 }
