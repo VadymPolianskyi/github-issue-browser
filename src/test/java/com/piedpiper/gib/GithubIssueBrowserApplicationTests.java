3a53144a2ecca487bf9a2f9a4d9964afe955983c
vadym <vadym.polyanski@gmail.com>
2017-11-06 20:20:18 +0200

    set false to annotationProcessing

--- src/test/java/com/piedpiper/gib/GithubIssueBrowserApplicationTests.java
+++ src/test/java/com/piedpiper/gib/GithubIssueBrowserApplicationTests.java
@@ -1,7 +1,15 @@
 package com.piedpiper.gib;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class GithubIssueBrowserApplicationTests {
 
+	@Test
 	public void contextLoads() {
 	}
 
