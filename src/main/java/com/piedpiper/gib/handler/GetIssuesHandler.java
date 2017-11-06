88a5dbba889d31e655d94006e8bd974ab3ded210
vadym <vadym.polyanski@gmail.com>
2017-11-07 01:19:47 +0200

    corrected handlers

--- src/main/java/com/piedpiper/gib/handler/GetIssuesHandler.java
+++ src/main/java/com/piedpiper/gib/handler/GetIssuesHandler.java
@@ -6,6 +6,7 @@ import com.piedpiper.gib.protocol.dao.IssueDao;
 import com.piedpiper.gib.service.GithubService;
 import com.piedpiper.gib.service.util.Mapper;
 import lombok.extern.slf4j.Slf4j;
+import org.kohsuke.github.GHIssue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -30,14 +31,20 @@ public class GetIssuesHandler implements Handler<GetIssuesRequest, GetIssuesResp
         log.debug("Got request for getting issues of {} {} repository of {} user. (page: {}, size: {})",
                request.getState(), request.getRepository(), request.getUser(), request.getPage(), request.getSize());
 
-        List<IssueDao> issues = githubService
+        List<GHIssue> ghIssues = githubService
                 .getIssues(request.getUser(), request.getRepository(),
-                        request.getState(), request.getPage(), request.getSize(), request.getToken())
-                .stream()
+                        request.getState(), request.getPage(), request.getSize(), request.getToken());
+
+        List<IssueDao> issues = ghIssues.stream()
                 .map(mapper::mapIssue)
                 .collect(Collectors.toList());
 
+
+//        int closedIssues = ghIssues.get(0).getMilestone().getClosedIssues();
+//        int openedIssues = ghIssues.get(0).getMilestone().getOpenIssues();
+
         log.info("Returned issues of repository with name '{}'.", request.getRepository());
+//        return new GetIssuesResponse(issues, closedIssues, openedIssues); todo: make it working
         return new GetIssuesResponse(issues);
     }
 }
