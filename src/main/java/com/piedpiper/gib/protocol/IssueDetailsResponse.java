7a9e15b717cb34f3adb670c3bed06d9ed49e0f37
vadym <vadym.polyanski@gmail.com>
2017-11-06 07:41:19 +0200

    hot fix for building

--- src/main/java/com/piedpiper/gib/protocol/IssueDetailsResponse.java
+++ src/main/java/com/piedpiper/gib/protocol/IssueDetailsResponse.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.piedpiper.gib.protocol.dao.IssueDetailDao;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
 public @Data
