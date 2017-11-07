3de2ad897a9b0389789bf43db4b3d2e5b0cbdedb
VadymPolyanski <vadym.polyanski@gmail.com>
2017-11-07 18:08:04 +0200

    corrected api

--- src/main/java/com/piedpiper/gib/protocol/FeatureIntegrationResponse.java
+++ src/main/java/com/piedpiper/gib/protocol/FeatureIntegrationResponse.java
@@ -1,16 +1,18 @@
 package com.piedpiper.gib.protocol;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
 public @Data
-class IssueDetailsRequest extends RepositoryRequest{
-    @JsonProperty("issue_number")
-    private int issueNumber;
+class FeatureIntegrationResponse extends Response {
+    private List<Integer> days;
+    private Integer avg;
+    private Integer count;
 }
