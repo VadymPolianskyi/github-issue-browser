1e278830c0ec6db87264d2b9dde6f915948a8e22
vadym <vadym.polyanski@gmail.com>
2017-11-07 08:44:05 +0200

    created feature integration service

---
+++ src/main/java/com/piedpiper/gib/protocol/FeautureIntegrationResponse.java
@@ -0,0 +1,17 @@
+package com.piedpiper.gib.protocol;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public @Data
+class FeautureIntegrationResponse extends Response {
+    private List<Integer> days;
+    private Integer avg;
+}
