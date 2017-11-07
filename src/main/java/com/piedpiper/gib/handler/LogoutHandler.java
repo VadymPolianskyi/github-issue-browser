3b9d64c24f5915ecc97bc5f72b9888b97b5f9012
VadymPolyanski <vadym.polyanski@gmail.com>
2017-11-07 10:03:21 +0200

    created endpoint for logout

---
+++ src/main/java/com/piedpiper/gib/handler/LogoutHandler.java
@@ -0,0 +1,29 @@
+package com.piedpiper.gib.handler;
+
+
+import com.piedpiper.gib.protocol.Response;
+import com.piedpiper.gib.protocol.TokenRequest;
+import com.piedpiper.gib.service.GithubService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Author: Vadym Polyanski
+ * Date: 07.11.17
+ * Time: 10:00
+ */
+@Component
+public class LogoutHandler implements Handler<TokenRequest, Response> {
+
+    private final GithubService githubService;
+
+    @Autowired
+    public LogoutHandler(GithubService githubService) {
+        this.githubService = githubService;
+    }
+
+    @Override
+    public Response handle(TokenRequest request) {
+        return null;
+    }
+}
